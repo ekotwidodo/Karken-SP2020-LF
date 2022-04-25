589dcbaab7770bcc87efb4139dc7fff5460da8ad
Eko Teguh Widodo <id.ekoteguh@gmail.com>
2022-04-25 14:33:23 +0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,212 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Kartu Kendali | Long Form SP2020</title>
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="style.css">
+
+</head>
+
+<body>
+    <h1 class="text-center">Contoh Form Kendali Long Form SP2020</h1>
+    <hr />
+    <div class="container">
+        <form id="form-add-lf-sp2020">
+            <table id="table-ruta-lf-sp2020" class="table table-responsive table-bordered">
+                <tbody>
+                    <tr style="background-color: #DEB6AB;">
+                        <td colspan="3">
+                            <b>BLOK I. IDENTIFIKASI RUMAH TANGGA SAMPEL</b>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style="width: 10px;">a.</td>
+                        <td style="width: 750px;">Kabupaten/Kota</td>
+                        <td>
+                            <select id="b1ra" name="b1ra" class="form-control">
+                                <option value="00">- Pilih Kabupaten/Kota -</option>
+                                <option value="01">Lampung Barat</option>
+                                <option value="02">Tanggamus</option>
+                                <option value="03">Lampung Selatan</option>
+                                <option value="04">Lampung Timur</option>
+                                <option value="05">Lampung Tengah</option>
+                                <option value="06">Lampung Utara</option>
+                                <option value="07">Way Kanan</option>
+                                <option value="08">Tulang Bawang</option>
+                                <option value="09">Pesawaran</option>
+                                <option value="10">Pringsewu</option>
+                                <option value="11">Mesuji</option>
+                                <option value="12">Tulang Bawang Barat</option>
+                                <option value="13">Pesisir Barat</option>
+                                <option value="71">Bandar Lampung</option>
+                                <option value="72">Metro</option>
+                            </select>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style="width: 10px;">b.</td>
+                        <td style="width: 750px;">Kecamatan</td>
+                        <td>
+                            <select id="b1rb" name="b1rb" class="form-control">
+                                <option value="000">- Pilih Kecamatan -</option>
+                                <option value="010">Labuhan Ratu</option>
+                            </select>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style="width: 10px;">c.</td>
+                        <td style="width: 750px;">Desa/Kelurahan</td>
+                        <td>
+                            <select id="b1rc" name="b1rc" class="form-control">
+                                <option value="000">- Pilih Desa/Kelurahan -</option>
+                                <option value="003">Labuhan Ratu Raya</option>
+                            </select>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style="width: 10px;">d.</td>
+                        <td style="width: 750px;">Nomor Blok Sensus (NBS)</td>
+                        <td>
+                            <select id="b1rd" name="b1rd" class="form-control">
+                                <option value="00">- Pilih NBS -</option>
+                                <option value="003B">003B</option>
+                            </select>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style="width: 10px;">e.</td>
+                        <td style="width: 750px;">Nomor Kode Sampel (NKS)</td>
+                        <td>
+                            <input type="text" id="b1re" name="b1re" class="form-control" value="1300014" />
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style="width: 10px;">f.</td>
+                        <td style="width: 750px;">Nomor Urut Rumah Tangga Sampel (NURTS)</td>
+                        <td>
+                            <select id="b1rf" name="b1rf" class="form-control">
+                                <option value="0">- Pilih NUS -</option>
+                                <option value="1">1</option>
+                                <option value="2">2</option>
+                                <option value="3">3</option>
+                                <option value="4">4</option>
+                                <option value="5">5</option>
+                                <option value="6">6</option>
+                                <option value="7">7</option>
+                                <option value="8">8</option>
+                                <option value="9">9</option>
+                                <option value="10">10</option>
+                                <option value="11">11</option>
+                                <option value="12">12</option>
+                                <option value="13">13</option>
+                                <option value="14">14</option>
+                                <option value="15">15</option>
+                            </select>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style="width: 10px;">g.</td>
+                        <td style="width: 750px;">Nama Kepala Rumah Tangga (KRT)</td>
+                        <td>
+                            <input type="text" id="b1rg" name="b1rg" class="form-control" value="EKO TEGUH WIDODO" />
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style="width: 10px;">h.</td>
+                        <td style="width: 750px;">Jumlah Anggota Rumah Tangga (n)</td>
+                        <td>
+                            <input type="number" id="b1rh" name="b1rh" class="form-control" min="1" step="1" max="20" />
+                        </td>
+                    </tr>
+                    <tr style="background-color: #DEB6AB;">
+                        <td colspan="3">
+                            <b>BLOK II. JUMLAH ANGGOTA RUMAH TANGGA</b>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colspan="3">
+                            <table class="table table-responsive table-bordered">
+                                <thead>
+                                    <tr>
+                                        <th scope="col" class="text-center">Nomor Anggota Rumah Tangga</th>
+                                        <th scope="col" class="text-center">Hubungan dengan KRT</th>
+                                        <th scope="col" class="text-center">Jenis Kelamin</th>
+                                        <th scope="col" class="text-center">Umur</th>
+                                        <th scope="col" class="text-center">Status Perkawinan</th>
+                                        <th scope="col" class="text-center">Nomor Urut Ibu Kandung</th>
+                                    </tr>
+                                </thead>
+                                <tbody id="table-art-lf-sp2020"></tbody>
+                            </table>
+                        </td>
+                    </tr>
+                    <tr style="background-color: #DEB6AB;">
+                        <td colspan="3">
+                            <b>BLOK III. LAINNYA</b>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style="width: 10px;">3.</td>
+                        <td style="width: 750px;">Apakah Ada Kejadian Migrasi Keluar Internasional di Rumah Tangga Ini? (Lihat P.501)</td>
+                        <td>
+                            <select id="b3r1" name="b3r1" class="form-control">
+                                <option value="0">- Pilih Jawaban -</option>
+                                <option value="1">Ya</option>
+                                <option value="2">Tidak</option>
+                            </select>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style="width: 10px;">4.</td>
+                        <td style="width: 750px;">Apakah Ada Kejadian Kematian di Rumah Tangga Ini? (Lihat P.601)</td>
+                        <td>
+                            <select id="b3r2" name="b3r2" class="form-control">
+                                <option value="0">- Pilih Jawaban -</option>
+                                <option value="1">Ya</option>
+                                <option value="2">Tidak</option>
+                            </select>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style="width: 10px;">5.</td>
+                        <td style="width: 750px;">Apakah Ada Kejadian Kelahiran di Rumah Tangga Ini? (Lihat P.701)</td>
+                        <td>
+                            <select id="b3r3" name="b3r3" class="form-control">
+                                <option value="0">- Pilih Jawaban -</option>
+                                <option value="1">Ya</option>
+                                <option value="2">Tidak</option>
+                            </select>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colspan="3">
+                            <button type="submit" class="btn btn-primary">KIRIM KE SERVER</button>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </form>
+    </div>
+
+    <!-- Optional JavaScript; choose one of the two! -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+    -->
+    <script src="script.js"></script>
+</body>
+
+</html>
---
+++ script.js
@@ -0,0 +1,133 @@
+$(document).ready(function() {
+
+    console.log('ready!');
+
+    $('#b1rh').on('keyup', function() {
+
+        // Ambil angka yang dipilih, tipe masih String
+        let b1rh = $('#b1rh').val();
+        
+        // Ubah ke dalam bentuk integer
+        const num_art = parseInt(b1rh);
+
+        // Apabila si user ketik selain angka
+        let tabel_art = '';
+
+        // Pengulangan row per tabel
+        for (let idx = 0; idx < num_art; idx++) {
+            tabel_art += 
+            `<tr>
+                <td style="width:10px;">
+                    <input type="number" id="b2a_${idx + 1}" name="b2a_${idx + 1}" class="form-control" value="${idx + 1}" disabled/>
+                </td>
+                <td>
+                    <select id="b2b_${idx + 1}" name="b2b_${idx + 1}" class="form-control">
+                        <option value="00">- Pilih Jawaban -</option>
+                        <option value="01">Kepala Rumah Tangga</option>
+                        <option value="02">Suami</option>
+                        <option value="03">Istri</option>
+                        <option value="04">Anak</option>
+                        <option value="05">Menantu</option>
+                        <option value="06">Cucu</option>
+                        <option value="07">Orang Tua</option>
+                        <option value="08">Mertua</option>
+                        <option value="09">Famili Lain</option>
+                        <option value="10">Pembantu/Sopir/Asisten Rumah Tangga/Pengasuh</option>
+                        <option value="11">Lainnya</option>
+                    </select>
+                </td>
+                <td>
+                    <select id="b2c_${idx + 1}" name="b2c_${idx + 1}" class="form-control">
+                        <option value="0">- Pilih Jawaban -</option>
+                        <option value="1">Laki-Laki</option>
+                        <option value="2">Perempuan</option>
+                    </select>
+                </td>
+                <td>
+                    <input type="number" id="b2d_${idx + 1}" name="b2d_${idx + 1}" class="form-control" min="1" step="1" />
+                </td>
+                <td>
+                    <select id="b2e_${idx + 1}" name="b2e_${idx + 1}" class="form-control">
+                        <option value="0">- Pilih Jawaban -</option>
+                        <option value="1">Belum Kawin</option>
+                        <option value="2">Kawin</option>
+                        <option value="3">Cerai Hidup</option>
+                        <option value="4">Cerai Mati</option>
+                    </select>
+                </td>
+                <td>
+                    <input type="text" id="b2f_${idx + 1}" name="b2f_${idx + 1}" class="form-control" min="0" step="1" />
+                </td>
+            </tr>`;
+        }
+        
+        $('#table-art-lf-sp2020').html(tabel_art);
+
+    });
+
+    // Fungsi untuk merge dua list menjadi dictionary object
+    function merged (first, second) {
+        let dict = {};
+        for (let i=0; i < first.length; i++) {
+            dict[first[i]] = second[i]
+        }
+
+        return dict;
+    }
+
+    // Submit
+    $('#form-add-lf-sp2020').submit(function(e) {
+        e.preventDefault();
+
+        let b1ra = $('#b1ra').val();
+        let b1rb = $('#b1rb').val();
+        let b1rc = $('#b1rc').val();
+        let b1rd = $('#b1rd').val();
+        let b1re = $('#b1re').val();
+        let b1rf = $('#b1rf').val();
+        let b1rg = $('#b1rg').val();
+        let b3r1 = $('#b3r1').val();
+        let b3r2 = $('#b3r2').val();
+        let b3r3 = $('#b3r3').val();
+
+        // Untuk b1rh, perlu perlakuan untuk ambil data
+        let b1rh = $('#b1rh').val();
+        let num_art = parseInt(b1rh);
+
+        let art = [];
+
+        $('#table-art-lf-sp2020 tr').each(function (idx, tr) {
+            // Mengambil cell value tiap row
+            let a = $(tr).find("td:eq(0) input[type='number']").val();
+            let b = $(tr).find("td:eq(1) select").val();
+            let c = $(tr).find("td:eq(2) select").val();
+            let d = $(tr).find("td:eq(3) input[type='number']").val();
+            let e = $(tr).find("td:eq(4) select").val();
+            let f = $(tr).find("td:eq(5) input[type='text']").val();
+            
+            // Memasukkannya ke dalam columns dan values
+            let columns = [`b2a_${idx + 1}`, `b2b_${idx + 1}`, `b2c_${idx + 1}`, `b2d_${idx + 1}`, `b2e_${idx + 1}`, `b2f_${idx + 1}`];
+            let values = [a, b, c, d, e, f];
+
+            art[idx] = merged(columns, values);
+        });
+
+        let ruta = {
+            'b1ra': b1ra,
+            'b1rb': b1rb,
+            'b1rc': b1rc,
+            'b1rd': b1rd,
+            'b1re': b1re,
+            'b1rf': b1rf,
+            'b1rg': b1rg,
+            'b1rh': num_art,
+            'b3r1': b3r1,
+            'b3r2': b3r2,
+            'b3r3': b3r3,
+            'art' : art
+        };
+
+        console.log(ruta)
+    });
+
+});
